c2b94c22faf22b1516cca701d8570b2184c553c1
yjl <yujianlin0427@163.com>
2018-07-19 18:43:06 +0800

    上传数据excel，可以生成

---
+++ WebRoot/pages/XlsFileUpload.jsp
@@ -0,0 +1,90 @@
+<%@ page contentType="text/html; charset=GBK" language="java"%>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+ <meta http-equiv="Content-Type" content="text/html; charset=gbk" />
+ <title>数据文件上传</title>
+ <script>
+     var xmlHttp;
+     function uploadxls(contextPath){
+         //判断是否选择了文件并且后缀是xls
+         if(check()){
+             //检验文件是否存在
+             xlsfileupload(contextPath)
+         }
+     }
+     //获取上传文件 文件名
+     function getFileName(){
+         //获取文件名
+         var upfile = document.getElementById("uploadxlsfile").value;
+         var allfilename = upfile.split("\\");
+         var filename = allfilename[allfilename.length-1];
+         return filename;
+     }
+     //
+     function xlsfileupload(contextPath){
+
+         document.uploadxlsform.action = contextPath+'/LoadReport';
+         //document.uploadxlsform.target="_self";
+         document.uploadxlsform.submit();
+
+         alert("通讯录更新完成！");
+         window.close();
+
+     }
+     function submitAfter(req,res){
+         var result = res.getAttr("result");
+         if(null!=result&&""!=result){
+             alert(result);
+         }
+     }
+     //是否选择了文件并且后缀是xls文件判断
+     function check(){
+         var upfile = document.getElementById("uploadxlsfile").value;
+         if(upfile=="" || upfile==null){
+             return false;
+         }
+         if(upfile.endWith("xls") ){
+             return true;
+         }else{
+             alert("请上传xls文件！");
+             return false;
+         }
+         return true;
+     }
+     String.prototype.endWith=function(oString){
+         var   reg=new RegExp(oString+"$");
+         return   reg.test(this);
+     }
+ </script>
+</head>
+<body>
+<p>
+<div align="center">
+ <form name="uploadxlsform" id="uploadxlsform" enctype="multipart/form-data" action="/ProduceExcel" method="post" >
+  <table border="0" cellpadding="0" cellspacing="0" width="100%" id="table1" class="table_style">
+   <tr class="table_th">
+    <td style="text-align:left">
+     &nbsp;&nbsp;请选择上传数据xls文件：
+    </td>
+   </tr>
+   <tr>
+    <td style="height:60px;">
+     <input type="file" name="uploadxlsfile" id="uploadxlsfile" placeholder="请选择数据文件" size="120" />
+    </td>
+   </tr>
+
+  </table>
+  <table>
+   <tr>
+    <td>
+     <input type="submit" value="提交" onclick="return check(this.form)" />
+     <%-- <input type="button" value="上 传" name="B1" class="button_common_skyBlue" onclick="uploadxls('<%=request.getContextPath()%>');" />
+     &nbsp;&nbsp; --%>
+     <input type="reset"  value="重 置" name="B2" class="button_common_skyBlue" />
+    </td>
+   </tr>
+  </table>
+ </form>
+</div>
+</body>
+</html>
--- src/com/jxlgnc/demo/Controller/ProduceExcel.java
+++ src/com/jxlgnc/demo/Controller/ProduceExcel.java
@@ -3,18 +3,49 @@ package com.jxlgnc.demo.Controller;
 import com.jxlgnc.demo.beans.Physical;
 import com.jxlgnc.demo.util.ExcelTool;
 import com.jxlgnc.demo.util.GetExcelInfo;
-
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileUploadException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.*;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
 /**
  * Created by yjl on 2018-07-19.
  */
-public class ProduceExcel {
+@WebServlet("/ProduceExcel")
+public class ProduceExcel extends HttpServlet{
+    File tempPathFile;
+    private String uploadPath = "E:\\ouDataTemp"; // 上传文件的目录
+    String fileName="";//默认文件名
 
     public static void main(String[] args) throws IOException {
+
+    }
+
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        uploadXls(request, response);
+        createExcel();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        uploadXls(request, response);
+        createExcel();
+    }
+
+    // 将一条条excel数据根据模板excel文件生成所有的excel
+    public void createExcel(){
         Map<String,Object> map=new HashMap<String,Object>();
         //这个是模板文件
         String path = "E:\\000.xls";
@@ -22,7 +53,7 @@ public class ProduceExcel {
 
         // 这个是excel数据文件：eg：E:/yujianlin/xxxxx.xls，必须是xls格式
         //如果后缀不是xls，请在excel另存为选择 xls 即可
-        File file = new File("E:/111.xls");
+        File file = new File(uploadPath+"/"+fileName);
         if(file==null||"".equals(file)){
             System.out.println("数据文件不存在，请检查文件是否存在");
             return;
@@ -52,7 +83,7 @@ public class ProduceExcel {
                 map.put("physical", physical);
 
                 in = ExcelTool.exportExcel(path, map);
-                out = new FileOutputStream("E:/ouData/余建林"+i+".xls");
+                out = new FileOutputStream(uploadPath+"/allexcel/余建林"+i+".xls");
                 BufferedInputStream bin = new BufferedInputStream(in);
                 BufferedOutputStream bout = new BufferedOutputStream(out);
                 while (true) {
@@ -87,8 +118,73 @@ public class ProduceExcel {
             }
 
         }
+    }
+
+    public void uploadXls(HttpServletRequest request,
+                          HttpServletResponse response) {
+        boolean isMultipart = ServletFileUpload.isMultipartContent(request);
+        if (!isMultipart) {
+            return;
+        }
+        File file = new File(uploadPath+"/allexcel");
+        //如果目录不存在
+        if (!file.isDirectory()) {
+            //创建文件上传目录
+            file.mkdirs();
+        }
+
+        // Create a factory for disk-based file items
+        DiskFileItemFactory factory = new DiskFileItemFactory();
+
+        // Set factory constraintsx
+        factory.setSizeThreshold(4096); //设置缓冲区大小
+        factory.setRepository(tempPathFile);// 设置缓冲区目录
+
+        // Create a new file upload handler
+        ServletFileUpload upload = new ServletFileUpload(factory);
 
 
+        // Parse the request
+        List<FileItem> items = null;
+        try {
+            items = upload.parseRequest(request);
+        } catch (FileUploadException e1) {
+            e1.printStackTrace();
+        }
 
+        // Process the uploaded items
+        Iterator<FileItem> iter = items.iterator();
+        while (iter.hasNext()) {
+            FileItem item = (FileItem)iter.next();
+
+            //整个表单的所有域都会被解析，要先判断一下是普通表单域还是文件上传域
+            if (item.isFormField()) {
+                String name = item.getFieldName();
+                String value = item.getString();
+                System.out.println(name + ":" + value);
+            } else {
+                String fieldName = item.getFieldName();
+                fileName = item.getName();
+                String contentType = item.getContentType();
+                boolean isInMem = item.isInMemory();
+                long sizeInBytes = item.getSize();
+                System.out.println(fieldName + ":" + fileName);
+                System.out.println("类型：" + contentType);
+                System.out.println("是否在内在：" + isInMem);
+                System.out.println("文件大小" + sizeInBytes);
+
+                //将文件保存到本地，暂时不用保存到本地
+                try {
+                    byte[] bytes = item.getName().getBytes();
+                    File fullfile = new File(new String(bytes, "utf-8"));
+                    File uploadfile = new File(uploadPath,fullfile.getName());
+                    item.write(uploadfile);
+
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+
+            }
+        }
     }
 }
